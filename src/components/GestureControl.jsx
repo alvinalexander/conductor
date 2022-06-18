df6fcf86b8ce245f90e21ae8561b86db90e66519
Nicolas Huynh Thien <nutella50g@gmail.com>
2022-06-17 23:24:53 -0700

    Add tooltip for each of the gestures
    - Add react-tooltip package
    - Add component and descriptions for each of the hand gestures

--- src/components/GestureControl.jsx
+++ src/components/GestureControl.jsx
@@ -5,6 +5,7 @@ import FormGroup from '@material-ui/core/FormGroup';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import FormHelperText from '@material-ui/core/FormHelperText';
 import Switch from '@material-ui/core/Switch';
+import ReactTooltip from 'react-tooltip';
 import { makeStyles } from '@material-ui/core/styles';
 import { Typography } from '@material-ui/core';
 
@@ -30,46 +31,55 @@ export default function GestureControl({onGestureToggle, gestures}) {
           Guestures
         </Typography>
       </FormLabel>
+      <ReactTooltip place="right" type="success" effect="solid" />
       <FormGroup>
         <FormControlLabel
           control={<Switch checked={gestures.xcontrol} onChange={onGestureToggle} name="xcontrol" />}
           label="X Control"
           labelPlacement="start"
+          data-tip="Send CC#16 using the horizontal position of your hand"
         />
         <FormControlLabel
           control={<Switch checked={gestures.ycontrol} onChange={onGestureToggle} name="ycontrol" />}
           label="Y Control"
           labelPlacement="start"
+          data-tip="Send CC#11 using the vertical position of your hand"
         />
         <FormControlLabel
           control={<Switch checked={gestures.spread} onChange={onGestureToggle} name="spread" />}
           label="Spread"
           labelPlacement="start"
+          data-tip="Send CC#66 using the spread of your fingers"
         />
         <FormControlLabel
           control={<Switch checked={gestures.rotation} onChange={onGestureToggle} name="rotation" />}
           label="Rotation"
           labelPlacement="start"
+          data-tip="Inactive"
         />
         <FormControlLabel
           control={<Switch checked={gestures.roll} onChange={onGestureToggle} name="roll" />}
           label="Roll"
           labelPlacement="start"
+          data-tip="Send CC#15 using the roll of your hand"
         />
          <FormControlLabel
           control={<Switch checked={gestures.proximity} onChange={onGestureToggle} name="proximity" />}
           label="Proximity"
           labelPlacement="start"
+          data-tip="Send CC#17 using the distance of your hand to the camera"
         />
         <FormControlLabel
           control={<Switch checked={gestures.pitch} onChange={onGestureToggle} name="pitch" />}
           label="Pitch"
           labelPlacement="start"
+          data-tip="Inactive"
         />
          <FormControlLabel
           control={<Switch checked={gestures.yaw} onChange={onGestureToggle} name="yaw" />}
           label="Yaw"
           labelPlacement="start"
+          data-tip="Send CC#16 using the yaw of your hand"
         />
       </FormGroup>
       <FormHelperText>Use this control what midi signals are generated</FormHelperText>
